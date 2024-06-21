a5d2b843d517b0a4fe15c4567c5f730de4211d41
mihireshjoshi <mihireshsjoshi@gmail.com>
2024-06-22 01:40:11 +0530

    Adding Qr initialised

---
+++ frontend/coin/component/Qr.js
@@ -0,0 +1,10 @@
+import { React, useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+
+const Qr = () => {
+  return (
+    <div>Qr</div>
+  )
+}
+
+export default Qr
