df8714d0369c1b72f9c97a7ef217ac780f7fb8c5
parthathalye17 <athalyeparth@gmail.com>
2024-06-22 03:12:11 +0530

    Fraud updated for receiver end

--- backend/parth/models/testing_model.py
+++ backend/parth/models/testing_model.py
@@ -3,16 +3,17 @@ import pandas as pd
 import joblib
 
 new_entry = {
-    "amount": 30000,
-    "transaction_hour": 2,
+    "amount": 3000,
+    "transaction_hour": 15,
     "transaction_day_of_week": 3,
-    "avg_transaction_amount": 521,
-    "city_consistency": 0.4,
+    "avg_transaction_amount": 5020,
+    "sender_city_consistency": 0.8,
+    "receiver_city_consistency":0.9,
     "country_consistency": 1,
-    "average_transaction_frequency": 1,
-    "transaction_type": 1,
+    "average_transaction_frequency": 2,
+    "transaction_type": 3,
     "transaction_day": 5,
-    "transaction_month": 2,
+    "transaction_month": 5,
     "transaction_year": 2024
 }
 
@@ -20,15 +21,14 @@ new_entry_df = pd.DataFrame([new_entry])
 xgb_model = joblib.load('xgb_model.pkl')
 scaler = joblib.load('scaler.pkl')
 
-columns = [ "amount","transaction_hour", "transaction_day_of_week",
-    "avg_transaction_amount", "city_consistency", "country_consistency", "average_transaction_frequency", "transaction_type",
-    "transaction_day", "transaction_month", "transaction_year"
+columns = [ "amount","transaction_hour", "transaction_day_of_week","avg_transaction_amount", "country_consistency", "average_transaction_frequency", "transaction_type",
+    "transaction_day", "transaction_month", "transaction_year","receiver_city_consistency","sender_city_consistency"
 ]
 new_entry_df = new_entry_df[columns]
 new_entry_scaled = scaler.transform(new_entry_df)
 
 y_pred_prob = xgb_model.predict_proba(new_entry_scaled)[:, 1]
-best_threshold = 0.5
+best_threshold = 0.82
 y_pred = (y_pred_prob > best_threshold).astype(int)
 
 print(f"Predicted probability of fraud: {y_pred_prob[0]}")
